2c84ed106f9cf8aa912ee0b13a0f15fdf6a17dbb
Armando Guardado <armando.guardado.ag@gmail.com>
2021-11-22 11:33:18 -0800

    Updated comments, removed debugging ( consloe.log()), and updated id for html elements to be in first positions.

--- app.js
+++ app.js
@@ -1,78 +1,85 @@
-const GOOGLE_DOODLE_ENDPOINT = "https://google-doodles.herokuapp.com/doodles/2020/11?hl=en";
-const cardDestination = document.getElementsByClassName("destination")[0];
-const title = document.getElementById('doodleTitle');
-const doodleButton = document.getElementById("moreDoodleButton");
-const scrollInputField = document.getElementById("scroll-date");
-const doodleInput= document.getElementById("inputDoodleDate");
-const scrollFeature = document.getElementById("scrollSearch");
-const inputFeature = document.getElementById("inputForm")
+const GOOGLE_SEARCH_HREF_TEMPLATE = "https://www.google.com/search?q=";
 
-const colorsHex = ['#4285f4', '#34a853', '#fbbc05', '#ea4335'];
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const GOOGLE_DOODLE_ENDPOINT = "https://google-doodles.herokuapp.com/doodles/2020/11?hl=en";
+
+const cardsContainer = document.getElementById("cards-container");
+const doodlesTitle = document.getElementById('doodleTitle');
+const getDoodlesButton = document.getElementById("moreDoodleButton");
+const scrollDateField = document.getElementById("scroll-date");
+const inputDateField = document.getElementById("inputDoodleDate");
+const scrollFeature = document.getElementById("scrollSearch");
+const inputFeature = document.getElementById("inputForm");
+
+const COLOR_HEX = ['#4285f4', '#34a853', '#fbbc05', '#ea4335'];
+const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const headerLetters = ["T", "o", "d", "a", "y", " ", "i", "n", " ", "G", "o", "o", "g", "l", "e", " ", "D", "o", "o", "d", "l", "e", "s", " ", "H", "i", "s", "t", "o", "r", "y"];
 
-// Will use as the starting day
+// Date object used to obtain present day and using this to show today's results.
+//Created new Date Object, using methods to get specific valus of Day, month, and Year (YYYY) format
 const initialToday = new Date();
-console.log(initialToday.toLocaleDateString());
 const initialDay = initialToday.getDate();
 const initialMonth = initialToday.getMonth() + 1;
 const initialYear = initialToday.getFullYear();
-console.log(initialDay);
-console.log(initialMonth);
-console.log(initialYear);
 
 
 
-
+//Function being used to create each indivudal Card and then appending result to cards Container to be displayed
 function createCard(imageUrl, title, date) {
     let cardElement = createElement("div", "card");
+
     let imgElement = createElement("img", "card-img-top card-style");
     adjustAttribute(imgElement, "src", imageUrl);
 
     let bodyElement = createElement("div", 'card-body');
     let titleElement = createElement("h5", "card-title", title);
     let dateElement = createElement("h5", "card-title", date);
+
     let anchorContainer = createElement("div", "d-grid col-12 mx-auto");
-
     let anchorElement = createElement("a", "btn btn-primary", "What in the doodle?");
-    adjustAttribute(anchorElement, "style", `background-color: ${getRandomColor()};`)
-    adjustAttribute(anchorElement, "href", createGoogleHref(title, date))
-    adjustAttribute(anchorElement, "target", "_blank")
-
-    anchorElement.addEventListener("mouseover", changeColor)
-    anchorElement.addEventListener("mouseout", changeColor)
+    adjustAttribute(anchorElement, "style", `background-color: ${getRandomColor()};`);
+    adjustAttribute(anchorElement, "href", createGoogleHref(title, date));
+    adjustAttribute(anchorElement, "target", "_blank");
+    anchorElement.addEventListener("mouseover", changeColor);
+    anchorElement.addEventListener("mouseout", changeColor);
 
     anchorContainer.append(anchorElement);
     bodyElement.append(titleElement, dateElement, anchorContainer)
     cardElement.append(imgElement, bodyElement);
 
-    cardDestination.append(cardElement);
+    cardsContainer.append(cardElement);
 }
 
-function createGoogleHref(title, date){
-    let searchUrl =  "https://www.google.com/search?q="
+//Function used to create href attribute value for each card using the fetch result values, these values contain spaces which neeed to be accounted for. 
+function createGoogleHref(title, date) {
     let customerHref;
     let customerTitle = title.replaceAll(' ', '%20');
     let customDate = date.replaceAll(' ', '%20');
-    customerHref =searchUrl + customerTitle + "%20" + customDate;
+    customerHref = GOOGLE_SEARCH_HREF_TEMPLATE + customerTitle + "%20" + customDate;
     return customerHref;
 }
 
+//Function that gets called when hovering over a button and dynamically changes its background color 
+//Uses the event object along with the event property to determine which button needs adjustment.
+//Also, uses the getRandomColor() to get random hex value from the colorHex array.
 function changeColor(e) {
-
     adjustAttribute(e.target, "style", `background-color: ${getRandomColor()};`);
 }
 
+//Function used to make attribute adjustments
+//Itthree parameters, the element, the attribute , and the value of the attribute
 function adjustAttribute(elem, attribute, value) {
     elem.setAttribute(attribute, value);
 }
 
+//Function that gets a randomColor from ColorHex array using the generateRandomIndex() that returns a random index. 
 function getRandomColor() {
-    let randomColorIndex = generateRandomIndex(colorsHex.length);
-
-    return colorsHex[randomColorIndex];
+    let randomColorIndex = generateRandomIndex(COLOR_HEX.length);
+    return COLOR_HEX[randomColorIndex];
 }
 
+//Function that is used to create elements dynamically, it take three parameters. 
+//string with the element that needs to be create, string with all the classes needed, and string with any innertText (optional)
+//returns a created element object
 function createElement(elementType, classes, innerText = "") {
     const element = document.createElement(elementType);
     element.className = classes;
@@ -82,23 +89,20 @@ function createElement(elementType, classes, innerText = "") {
     return element;
 }
 
-
-
+//Main function that gets triggered to start creating the cardsContainer
 function getDoodle(day, month, year) {
     let promises = [];
 
     for (let index = year; 1998 <= index; index--) {
         promises.push(makeFetchCall(index, month));
     }
+
     Promise.all(promises)
         .then((results) => {
-            // console.log(results[0]);
             results.forEach(element => {
-                console.log(element);
                 element.forEach(item => {
                     let item2 = item.run_date_array;
                     if (item2[1] === month && item2[2] === day) {
-                        console.log(item2);
                         let customDate = createCustomDate(item2[2], item2[1], item2[0]);
                         createCard(item.url, item.query, customDate);
                     }
@@ -108,178 +112,129 @@ function getDoodle(day, month, year) {
 
 }
 
+//async function that makes the fetch call and returns a json object with results
 async function makeFetchCall(doodleYear, doodleMonth) {
     let response1 = await fetch(`https://google-doodles.herokuapp.com/doodles/${doodleYear}/${doodleMonth}?hl=en`);
     let json = await response1.json();
     return json;
 }
 
+//function that getsa random integer based on the parameter passed in.  range [0, size) 
 function generateRandomIndex(size) {
-    return Math.floor(Math.random() * size)
+    return Math.floor(Math.random() * size);
 }
 
+// function used to create custom String formatted Date for displaying purposes.
 function createCustomDate(day, month, year) {
-    let dateString = months[month - 1] + " " + day + ", " + year;
-    return dateString
+    let dateString = MONTHS[month - 1] + " " + day + ", " + year;
+    return dateString;
 }
 
-
+//Function used when "get Doodles" button gets presses and uses value in input field as query values
 function getNewDoodle(form) {
     dateResult = new Date(form.date.value);
-    console.log(form.date.value)
-    console.log(typeof (form.date.value))
     let textArray = form.date.value.split("/");
-    console.log(parseInt(textArray[0]))
-    console.log(textArray[1])
-    console.log(textArray[2])
-
     let day = parseInt(textArray[1]);
     let month = parseInt(textArray[0]);
     let year = parseInt(textArray[2]);
-    clearDestination();
-    getDoodle(day, month, year)
-    scrollInputField.innerText = createCustomDate(dateResult.getDate(), dateResult.getMonth() + 1, dateResult.getFullYear());
+    clearCardsContainer();
+    getDoodle(day, month, year);
+    scrollDateField.innerText = createCustomDate(dateResult.getDate(), dateResult.getMonth() + 1, dateResult.getFullYear());
 }
 
-
-function clearDestination() {
-    let element = cardDestination;
+// function used to clear cardContainer of child elements
+function clearCardsContainer() {
+    let element = cardsContainer;
     while (element.firstChild) {
         element.removeChild(element.firstChild);
     }
 }
 
+//Function called to set up Header (title)
 function setUpHeader() {
     headerLetters.forEach((letter) => {
-        let tempElement = createElement("span", "textFormat", letter)
-        adjustAttribute(tempElement, "style", `color: ${getRandomColor()};`)
-        title.appendChild(tempElement)
+        let tempElement = createElement("span", "textFormat", letter);
+        adjustAttribute(tempElement, "style", `color: ${getRandomColor()};`);
+        doodlesTitle.appendChild(tempElement);
     })
 }
 
+//function used to set up elements with id's -->  scrollSearch and inputForm 
 function setUpSearchElement() {
-    adjustAttribute(doodleButton, "style", `Background-color: ${getRandomColor()};`);
-    doodleButton.addEventListener("mouseover", changeColor);
-    doodleButton.addEventListener("mouseout", changeColor);
+    adjustAttribute(getDoodlesButton, "style", `Background-color: ${getRandomColor()};`);
+    getDoodlesButton.addEventListener("mouseover", changeColor);
+    getDoodlesButton.addEventListener("mouseout", changeColor);
 
     let rightButton = document.getElementById("right-button");
     rightButton.addEventListener("mouseover", changeColor);
     rightButton.addEventListener("mouseout", changeColor);
     rightButton.addEventListener("click", getNextDay);
+
     let leftButton = document.getElementById("left-button");
     leftButton.addEventListener("click", getPreviousDay);
     leftButton.addEventListener("mouseover", changeColor);
     leftButton.addEventListener("mouseout", changeColor);
-    scrollInputField.innerText = createCustomDate(initialDay, initialMonth , initialYear);
-    scrollInputField.addEventListener("mouseover", unhideInputDate)
-    inputFeature.addEventListener("mouseout", hideInputDate )
-    doodleInput.value = initialToday.toLocaleDateString();
-    
-}
 
-function unhideInputDate(){
-    scrollFeature.classList.add("display-feature")
-    inputFeature.classList.remove("display-feature")
+    scrollDateField.innerText = createCustomDate(initialDay, initialMonth, initialYear);
+    inputDateField.value = initialToday.toLocaleDateString();
+
+    scrollDateField.addEventListener("mouseover", unhideInputDate);
+    inputFeature.addEventListener("mouseout", hideInputDate);
 
 
 }
-function hideInputDate(){
-    scrollFeature.classList.remove("display-feature")
-    inputFeature.classList.add("display-feature")
+
+//fuunction used to unhide element with id= inputForm and hide element with id=scrollSearch
+function unhideInputDate() {
+    scrollFeature.classList.add("display-feature");
+    inputFeature.classList.remove("display-feature");
+
 
 }
 
-function getNextDay(){
-    let stringValue = scrollInputField.innerText;
+//fuunction used to hide element with id= inputForm and unhide element with id=scrollSearch
+function hideInputDate() {
+    scrollFeature.classList.remove("display-feature");
+    inputFeature.classList.add("display-feature");
+
+}
+
+//function that get triggered when user clicks on the right button
+function getNextDay() {
+    let stringValue = scrollDateField.innerText;
     let currentDay = new Date(stringValue);
-    let nextDay =  new Date(currentDay);
-    nextDay.setDate(currentDay.getDate() + 1);  
-    let day = nextDay.getDate();  
+    let nextDay = new Date(currentDay);
+    nextDay.setDate(currentDay.getDate() + 1);
+    let day = nextDay.getDate();
     let month = nextDay.getMonth() + 1;
     let year = nextDay.getFullYear();
-    scrollInputField.innerText = createCustomDate(day, month , year);
-    doodleInput.value = nextDay.toLocaleDateString();
-    clearDestination();
+    scrollDateField.innerText = createCustomDate(day, month, year);
+    inputDateField.value = nextDay.toLocaleDateString();
+    clearCardsContainer();
     getDoodle(day, month, year);
 }
 
-function getPreviousDay(){
-    let stringValue = scrollInputField.innerText;
+//Function that gets triggered when user clicks on left button
+function getPreviousDay() {
+    let stringValue = scrollDateField.innerText;
     let currentDay = new Date(stringValue);
-    let previousDay =  new Date(currentDay);
+    let previousDay = new Date(currentDay);
     previousDay.setDate(currentDay.getDate() - 1);
     let day = previousDay.getDate();
     let month = previousDay.getMonth() + 1;
     let year = previousDay.getFullYear();
-    scrollInputField.innerText = createCustomDate(day, month , year);
-    doodleInput.value = previousDay.toLocaleDateString();
-    clearDestination();
+    scrollDateField.innerText = createCustomDate(day, month, year);
+    inputDateField.value = previousDay.toLocaleDateString();
+    clearCardsContainer();
     getDoodle(day, month, year);
 }
 
+//function that is used to set up initial elements and triggers for cardsContainer to be populated with todays date object
 function start() {
     setUpSearchElement();
     setUpHeader();
     getDoodle(initialDay, initialMonth, initialYear);
 }
 
-
-
+//call to start()
 start();
-
-// console.log(createGoogleHref("Museo del Prado", "November 19 2020"));
-
-
-
-
-
-
-
-
-
-
-//Old code that did not work 
-
-
-// fetch(`https://google-doodles.herokuapp.com/doodles/${year}/11?hl=en`).then((res) => {
-//     console.log(res.ok)
-//     if (!res.ok) {
-//         throw Error(res.statusText)
-//     } else {
-//         return res.json();
-//     }
-// }).then((doodleResult) => {
-//     // console.log(doodleResult);
-//     doodleResult.forEach(element => {
-//         let item = element.run_date_array;
-//         // console.log(typeof(item[0]));
-//         // console.log(item[1]);
-//         // console.log(item[2])
-//         // console.log(item[0] === year);
-//         // console.log(item[1]=== month )
-//         // console.log(item[2] == day)
-
-//         if (item[1] === month && item[2] == day) {
-//             // console.log(item)
-//             createCard(element.url, item[0].toString(), item[2].toString())
-//         }
-
-//     });
-
-// }).catch((error) => {
-//     console.log(error)
-// });
-// year = year - 1;
-
-
-// function makeFetchCall(dodleYear) {
-//     fetch(`https://google-doodles.herokuapp.com/doodles/2020/11?hl=en`).then((res) => {
-//         console.log(res.ok)
-//         if (!res.ok) {
-//             throw Error(res.statusText)
-//         } else {
-//             return res.json();
-//         }
-//     }).then((doodleResult) => console.log(doodleResult))
-//         .catch((error) => console.log(error));
-// }
